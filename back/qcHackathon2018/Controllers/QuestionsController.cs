84f4260e468c6f32587ac8f9de0844aaa156b513
Tanzim Ali <tanzim_rayhan@yahoo.com>
2018-10-27 17:10:06 -0400

    added back end

---
+++ back/qcHackathon2018/Controllers/QuestionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace qcHackathon2018.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
